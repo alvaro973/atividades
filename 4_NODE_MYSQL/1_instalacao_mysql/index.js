681e78b6952ef1115d7cec648bcb86181f4cd0be
Alvaro Augusto <alvaroaugusto7a2@gmail.com>
2023-10-02 19:50:06 -0300

    primeira aula

--- 4_NODE_MYSQL/1_instalacao_mysql/index.js
+++ 4_NODE_MYSQL/1_instalacao_mysql/index.js
@@ -4,10 +4,28 @@ const mysql = require("mysql")
 
 const app = express()
 
+app.engine('hendlebars', exphbs.engine())
+app.set('view engine', 'hendlebars') 
+app.get('/', (req, res)=>{
+    res.render('home')
+
+})
 const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
     database: "nodemysql",
     port: 3307
+})
+conn.connect((error) => {
+    if (error) {
+        console.log(error)
+        return
+    }
+    console.log('Conectou a mySQl')
+    app.listen(3000, () =>{
+        console.log('Servidor rodando na porta 30000')
+    }
+    )
+
 })
